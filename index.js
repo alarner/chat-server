85393a4374466210e234885826899c0989e26de8
Brandon Hill <brandon@climbalytics.com>
2020-08-26 11:38:14 -0500

    emoji reactions, edit of messages and a delete endpoint that doesn't work

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const messages = [];
+const reactions = [];
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -31,12 +32,53 @@ app.post("/message", (req, res) => {
   res.json(message);
 });
 
+app.post("/message/:id/reaction", (req, res) => {
+  if (!req.body.user) {
+    return res.status(400).json({ error: "Missing user" });
+  }
+  if (!req.body.reaction) {
+    return res.status(400).json({ error: "Missing reaction" });
+  }
+  const reaction = {
+    user: req.body.user,
+    reaction: req.body.reaction,
+    createdAt: new Date(),
+  };
+  reactions.push(reaction);
+  res.json(reaction);
+});
+
 app.get("/messages", (req, res) => {
   const afterId = req.query.afterId || 0;
   if (parseInt(afterId) != afterId) {
     res.status(400).json({ error: "Invalid afterId" });
   }
   res.json(messages.slice(afterId));
+});
+
+app.put("/message/:id", (req, res) => {
+  if (!messages[req.params.id - 1]) {
+    return res.status(404).json({ error: "Unknown item" });
+  }
+  if (req.body.message) {
+    messages[req.params.id - 1].message = req.body.message;
+  }
+  if (req.body.user) {
+    messages[req.params.id - 1].user = req.body.user;
+  }
+  res.json(messages[req.params.id - 1]);
+});
+
+app.delete("/message/:id", (req, res) => {
+  // doesn't work
+  if (!messages[req.params.id - 1]) {
+    return res.status(404).json({ error: "Unknown item" });
+  }
+  const index = messages.indexOf(req.body.message);
+  if (req.body.message) {
+    messages.splice(index, 1);
+  }
+  res.json({ message: "message deleted" });
 });
 
 app.listen(port, () => {
