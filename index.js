b42ccb37ed0f60b4e25b5fa400aa1e156f30c703
Brandon Hill <brandon@climbalytics.com>
2020-08-27 11:51:27 -0500

    created a GET reactions end point and added a check for reaction duplications in the POST

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 let nextId = 1;
+let reactionNextId = 1;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const messages = [];
-const reactions = [];
+let messages = [];
+let reactions = [];
+let usersReactions = [];
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -39,13 +41,41 @@ app.post("/message/:id/reaction", (req, res) => {
   if (!req.body.reaction) {
     return res.status(400).json({ error: "Missing reaction" });
   }
-  const reaction = {
-    user: req.body.user,
-    reaction: req.body.reaction,
-    createdAt: new Date(),
-  };
-  reactions.push(reaction);
-  res.json(reaction);
+  // handling duplicate user reactions
+  function compareReactions(e) {
+    const userReaction = req.body.reaction;
+    const user = req.body.user;
+    const messageId = parseInt(req.params.id);
+    if (
+      e.user === user &&
+      e.reaction === userReaction &&
+      e.messageId === messageId
+    ) {
+      return true;
+    }
+  }
+  if (reactions.some(compareReactions)) {
+    res.status(400).json({ error: " Duplicate reaction" });
+  } else {
+    const reaction = {
+      id: reactionNextId++,
+      messageId: parseInt(req.params.id),
+      user: req.body.user,
+      reaction: req.body.reaction,
+      createdAt: new Date(),
+    };
+    reactions.push(reaction);
+    res.json(reaction);
+  }
+});
+
+app.get("/message/:id/reactions", (req, res) => {
+  const mId = parseInt(req.params.id);
+  function messageUserReactions(e) {
+    return e.messageId === mId;
+  }
+  let messageReactions = reactions.filter(messageUserReactions);
+  res.json(messageReactions);
 });
 
 app.get("/messages", (req, res) => {
